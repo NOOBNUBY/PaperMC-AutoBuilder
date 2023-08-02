91b2b6bc0662b450c569b102fd10b1eb99f19e07
NOOBNUBY <noobnuby77@gmail.com>
2023-08-02 10:35:47 +0900

    TODO 이미지

--- src/main/kotlin/com/noobnuby/Main.kt
+++ src/main/kotlin/com/noobnuby/Main.kt
@@ -3,10 +3,19 @@ package com.noobnuby
 import java.awt.*
 import javax.swing.*
 
-
 fun main(args: Array<String>) {
+
+    //TODO 이미지
     val frame = JFrame("PaperMCAutoBuilder")
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     frame.size = Dimension(300, 500)
+    //Create main img
+    val icon = ImageIcon("resources/image.png")
+    frame.iconImage = icon.image
+    //Create installButton
+    val installButton = JButton("Install")
+    installButton.preferredSize = Dimension(10, 10)
+
+    frame.add(installButton,BorderLayout.SOUTH)
     frame.isVisible = true
 }
